a48eecb2d22ba08970b613642da4eb6693c03bb6
amalhajoF <57699485+amalhajoF@users.noreply.github.com>
2019-11-13 09:20:53 +0300

    Add files via upload

---
+++ build/precache-manifest.969c862ff8f88e38d7bb5be92a149d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b3a32dcc4d03c273bc3acd48ceabf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4562ca5adad6ce19de5a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "26c289c65a90b49f72a1",
+    "url": "/static/js/2.1794e528.chunk.js"
+  },
+  {
+    "revision": "4562ca5adad6ce19de5a",
+    "url": "/static/js/main.8039d3d1.chunk.js"
+  },
+  {
+    "revision": "8aa7a348f1276dc84ebb",
+    "url": "/static/js/runtime-main.33ef0c94.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
